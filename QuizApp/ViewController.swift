b7f9145eca4e85901b6fa07b6a58aa856ea65b10
chihyin wang <a36980097@gmail.com>
2020-07-02 17:17:22 +0800

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by chihyin wang on 2020/7/2.
+//  Copyright © 2020 chihyinwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
