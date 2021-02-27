cea520c5170616848b563c981a2e9338045ad76c
chihyin wang <a36980097@gmail.com>
2021-02-27 11:14:39 +0800

    Add interactable SingleAnswerQuestionTestView preview

--- App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -15,7 +15,9 @@ struct SingleAnswerQuestion: View {
             QuestionHeader(title: title, question: question)
             
             ForEach(options, id: \.self) { option in
-                SingleTextSelectionCell(text: option, selection: {})
+                SingleTextSelectionCell(text: option, selection: {
+                    selection(option)
+                })
             }
             
             Spacer()
@@ -26,27 +28,31 @@ struct SingleAnswerQuestion: View {
 struct SingleAnswerQuestion_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            SingleAnswerQuestion(
-                title: "1 of 2",
-                question: "What's Bill's favorite color?",
-                options: [
-                    "Maroon",
-                    "Navy",
-                    "Black"
-                ], selection: { _ in }
-            )
+            SingleAnswerQuestionTestView()
             
-            SingleAnswerQuestion(
-                title: "1 of 2",
-                question: "What's Bill's favorite color?",
-                options: [
-                    "Maroon",
-                    "Navy",
-                    "Black"
-                ], selection: { _ in }
-            )
-            .preferredColorScheme(.dark)
-            .environment(\.sizeCategory, .extraExtraExtraLarge)
+            SingleAnswerQuestionTestView()
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .extraExtraExtraLarge)
+        }
+    }
+    
+    struct SingleAnswerQuestionTestView: View {
+        @State var selection: String = "none"
+        
+        var body: some View {
+            VStack {
+                SingleAnswerQuestion(
+                    title: "1 of 2",
+                    question: "What's Bill's favorite color?",
+                    options: [
+                        "Maroon",
+                        "Navy",
+                        "Black"
+                    ], selection: { selection = $0 }
+                )
+                
+                Text("Last selection " + selection)
+            }
         }
     }
 }
