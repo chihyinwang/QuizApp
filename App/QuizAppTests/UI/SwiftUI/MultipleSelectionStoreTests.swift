0638ef233245eb5a3cd5ac83e1b572f89c906527
chihyin wang <a36980097@gmail.com>
2021-03-03 23:06:55 +0800

    Toggle selection state

---
+++ App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -0,0 +1,37 @@
+//
+// Copyright © 2021 chihyinwang. All rights reserved.
+//
+
+import XCTest
+
+struct MultipleSelectionStore {
+    var options: [MultipleSelectionOption]
+    
+    init(options: [String]) {
+        self.options = options.map { MultipleSelectionOption(text: $0) }
+    }
+}
+
+struct MultipleSelectionOption {
+    let text: String
+    var isSelected = false
+    
+    mutating func select() {
+        isSelected.toggle()
+    }
+}
+
+class MultipleSelectionStoreTests: XCTestCase {
+    
+    func test_() {
+        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        XCTAssertFalse(sut.options[0].isSelected)
+
+        sut.options[0].select()
+        XCTAssertTrue(sut.options[0].isSelected)
+        
+        sut.options[0].select()
+        XCTAssertFalse(sut.options[0].isSelected)
+    }
+    
+}
