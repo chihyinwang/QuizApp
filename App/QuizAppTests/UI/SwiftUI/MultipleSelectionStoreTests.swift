1c922f718956c199164c5c67da28feea3fc4bdb9
chihyin wang <a36980097@gmail.com>
2021-03-04 10:18:35 +0800

    Can submit when at least one option is selected

--- App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -7,6 +7,10 @@ import XCTest
 struct MultipleSelectionStore {
     var options: [MultipleSelectionOption]
     
+    var canSubmit: Bool {
+        !options.filter(\.isSelected).isEmpty
+    }
+    
     init(options: [String]) {
         self.options = options.map { MultipleSelectionOption(text: $0) }
     }
@@ -34,4 +38,17 @@ class MultipleSelectionStoreTests: XCTestCase {
         XCTAssertFalse(sut.options[0].isSelected)
     }
     
+    func test_canSubmit_whenAtLeastOneOptionIsSelected() {
+        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        XCTAssertFalse(sut.canSubmit)
+
+        sut.options[0].select()
+        XCTAssertTrue(sut.canSubmit)
+        
+        sut.options[0].select()
+        XCTAssertFalse(sut.canSubmit)
+        
+        sut.options[1].select()
+        XCTAssertTrue(sut.canSubmit)
+    }
 }
