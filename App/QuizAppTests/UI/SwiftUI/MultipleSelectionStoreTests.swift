4dda1525aa7a74698f6540806df62c02048caf76
chihyin wang <a36980097@gmail.com>
2021-03-04 10:13:16 +0800

    Add test name

--- App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -23,7 +23,7 @@ struct MultipleSelectionOption {
 
 class MultipleSelectionStoreTests: XCTestCase {
     
-    func test_() {
+    func test_selectionOption_toggleState() {
         var sut = MultipleSelectionStore(options: ["o0", "o1"])
         XCTAssertFalse(sut.options[0].isSelected)
 
