9de214ee3ccbfa4645070e964eb27b0171878f1f
chihyin wang <a36980097@gmail.com>
2021-03-18 11:59:48 +0800

    Add AppDelegate to demonstrate we can change UI framework whenever we want

---
+++ App/QuizApp/Main/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+// Copyright © 2021 chihyinwang. All rights reserved.
+//
+
+import UIKit
+import QuizEngine
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    var quiz: Quiz?
+    
+    private lazy var navigationController = UINavigationController()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        startNewQuiz()
+        
+        return true
+    }
+    
+    private func startNewQuiz() {
+        let factory = iOSUIKitViewControllerFactory(options: options, correctAnswers: correctAnswers)
+        let router = NavigationControllerRouter(navigationController, factory: factory)
+
+        quiz = Quiz.start(questions: questions, delegate: router)
+    }
+}
