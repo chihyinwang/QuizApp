93bea9e7d6b90d7036fba4554aec65e83758493c
chihyin wang <a36980097@gmail.com>
2021-03-22 16:42:38 +0800

    Prevent duplicate options for question

--- App/QuizAppTests/Scoring/BasicQuizBuilderTests.swift
+++ App/QuizAppTests/Scoring/BasicQuizBuilderTests.swift
@@ -13,13 +13,27 @@ struct BasicQuiz {
 struct NonEmptyOptions {
     let head: String
     let tail: [String]
+    
+    var all: [String] {
+        [head] + tail
+    }
 }
 
 struct BasicQuizBuilder {
     private let questions: [Question<String>]
     private let options: [Question<String>: [String]]
+    
+    enum AddingError: Equatable, Error {
+        case duplicateOptions([String])
+    }
 
-    init(singleAnswerQuestion: String, options: NonEmptyOptions) {
+    init(singleAnswerQuestion: String, options: NonEmptyOptions) throws {
+        let allOptions = options.all
+        
+        guard Set(allOptions).count == allOptions.count else {
+            throw AddingError.duplicateOptions(allOptions)
+        }
+        
         let question = Question.singleAnswer(singleAnswerQuestion)
         self.questions = [question]
         self.options = [question: [options.head] + options.tail]
@@ -32,14 +46,27 @@ struct BasicQuizBuilder {
 
 class BasicQuizBuilderTests: XCTestCase {
     
-    func test_initWithSingleAnswerQuestion() {
-        let sut = BasicQuizBuilder(
+    func test_initWithSingleAnswerQuestion() throws {
+        let sut = try BasicQuizBuilder(
             singleAnswerQuestion: "q1",
             options: NonEmptyOptions(head: "o1", tail: ["o2", "o3"]))
         
         let result = sut.build()
         XCTAssertEqual(result.questions, [.singleAnswer("q1")])
         XCTAssertEqual(result.options, [.singleAnswer("q1"): ["o1", "o2", "o3"]])
+    }
+    
+    func test_initWithSingleAnswerQuestion_duplicateOptions_throw() throws {
+        XCTAssertThrowsError(
+            try BasicQuizBuilder(
+                singleAnswerQuestion: "q1",
+                options: NonEmptyOptions(head: "o1", tail: ["o1", "o3"]))
+        ) { error in
+            XCTAssertEqual(
+                error as? BasicQuizBuilder.AddingError,
+                BasicQuizBuilder.AddingError.duplicateOptions(["o1", "o1", "o3"])
+            )
+        }
     }
     
 }
