f19cdd90da92352a6bb59365bce08a5420c394d8
chihyin wang <a36980097@gmail.com>
2021-03-31 12:06:01 +0800

    Replace hardcoded primitives with BasicQuiz struct created with the new builder

--- App/QuizApp/Main/DemoQuizData.swift
+++ App/QuizApp/Main/DemoQuizData.swift
@@ -4,20 +4,20 @@
 
 import QuizEngine
 
-let question1 = Question.singleAnswer("What's Bill's favorite color?")
-let question2 = Question.multipleAnswer("What's Bill's favorite brand?")
-let questions = [question1, question2]
-
-let option101 = "Maroon"
-let option102 = "Navy"
-let option103 = "Black"
-let options1 = [option101, option102, option103]
-
-let option201 = "Maison Martin Margiela"
-let option202 = "Christian Dior"
-let option203 = "Raf Simons"
-let option204 = "Haider Ackermann"
-let options2 = [option201, option202, option203, option204]
-
-let options = [question1: options1, question2: options2]
-let correctAnswers = [(question1, [option103]), (question2, [option201, option202, option204])]
+let demoQuiz = try!
+    BasicQuizBuilder(
+        singleAnswerQuestion: "What's Mike's nationality?",
+        options: .init(head: "Canadian", tail: ["American", "Greek"]),
+        answer: "Greek"
+    )
+    .adding(
+        multipleAnswerQuestion: "What are Caio's nationalities?",
+        options: .init(head: "Portuguese", tail: ["American", "Brazilian"]),
+        answer: .init(head: "Portuguese", tail: ["Brazilian"])
+    )
+    .adding(
+        singleAnswerQuestion: "What's the capital of Brazil?",
+        options: .init(head: "Sao Paulo", tail: ["Rio de Janeiro", "Brasilia"]),
+        answer: "Brasilia"
+    )
+    .build()
